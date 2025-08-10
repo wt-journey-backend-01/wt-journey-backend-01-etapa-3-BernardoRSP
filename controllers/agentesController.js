ce4b623be7e7a903efb24f7c90a2c522b1d174d6
Bernardo Rezende <bernardo.com5@gmail.com>
2025-08-10 14:59:08 -0300

    Tratei as DatasDeIncorporação para serem exibidadas no padrão correto em Atualizar e Adicionar agente

--- controllers/agentesController.js
+++ controllers/agentesController.js
@@ -106,6 +106,7 @@ async function atualizarAgente(req, res) {
     }
 
     const agenteAtualizado = await agentesRepository.atualizar({ nome, dataDeIncorporacao, cargo }, id);
+    agenteAtualizado.dataDeIncorporacao = new Date(agenteAtualizado.dataDeIncorporacao).toISOString().split("T")[0];
     if (!agenteAtualizado) {
       return res.status(404).json({ status: 404, mensagem: "Agente não encontrado" });
     }
